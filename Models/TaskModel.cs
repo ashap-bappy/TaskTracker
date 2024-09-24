9e7fad52a508e55c30c3d38be06ac76393e780b1
ashap-bappy <bappy.cse08@gmail.com>
2024-09-24 23:04:57 +0600

    Add project files.

---
+++ Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using TaskTracker.Enums;
+
+namespace TaskTracker.Models
+{
+    public class TaskModel
+    {
+        public required string Id { get; set; }
+        public string? Description { get; set; }
+        public Status TaskStatus { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Text.Json;
+using TaskTracker.Enums;
+using TaskTracker.Models;
+
+namespace TaskTracker
+{
+    public class Program
+    {
+        static string directoryPath = @"D:\Educational\Code\Projects\TaskTracker\Data\";
+        static string fileName = "tasks.json";
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Invalid command!!!");
+                return;
+            }
+
+            var command = args[0];
+
+            switch(command)
+            {
+                case "add":
+                    AddTask(args);
+                    break;
+            }
+        }
+
+        private static void AddTask(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Invalid command!!! Task name is missing.");
+                return;
+            }
+            var task = new TaskModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                Description = args[1],
+                TaskStatus = Status.Todo,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+            var path = Path.Combine(directoryPath, fileName);
+            string jsonString = JsonSerializer.Serialize(task, new JsonSerializerOptions { WriteIndented = true});
+            File.WriteAllText(path, jsonString);
+        }
+    }
+}
